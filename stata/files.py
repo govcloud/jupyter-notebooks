9de543a799856b5b531488f96263e2044fab1c52
William Hearn <sylus1984@gmail.com>
2018-06-11 14:43:09 -0400

    feat(sas): Modify SAS configuration

---
+++ stata/files.py
@@ -0,0 +1,6 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+import ipystata
+from ipystata.config import config_stata
+config_stata('/home/user/stata14/stata-se')
