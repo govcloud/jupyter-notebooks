2add43694ba459eeace95f7666d0bf3853b41f7f
William Hearn <sylus1984@gmail.com>
2018-08-20 08:43:03 -0400

    feat(storage): Add blobfuse to remaining jupyter notebook(s)

---
+++ scipy/scripts/mnt-blobfuse.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+IFS=$'\n\t'
+
+export AZURE_MOUNT_POINT=/home/$NB_USER/wasb
+
+# mount our blobstore
+test ${AZURE_MOUNT_POINT}
+rm -rf ${AZURE_MOUNT_POINT}
+mkdir -p ${AZURE_MOUNT_POINT}
+
+if [[ ! -z "${AZURE_STORAGE_ACCOUNT}" && ! -z "${AZURE_STORAGE_ACCOUNT_CONTAINER}" ]] && [[ ! -z "${AZURE_STORAGE_ACCESS_KEY}" || ! -z "${AZURE_STORAGE_SAS_TOKEN}" ]]; then
+  blobfuse ${AZURE_MOUNT_POINT} --use-https=true --tmp-path=/tmp/blobfuse/${AZURE_STORAGE_ACCOUNT} --container-name=${AZURE_STORAGE_ACCOUNT_CONTAINER} -o allow_other
+fi
