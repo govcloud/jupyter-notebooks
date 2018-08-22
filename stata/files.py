9cbf00f48a063b0bdbbd87ae300d75df20d756c3
William Hearn <sylus1984@gmail.com>
2018-08-21 21:42:15 -0400

    feat(stata): Simplify image layers / sql odbc driver

--- stata/files.py
+++ stata/files.py
@@ -4,4 +4,6 @@
 import ipystata
 from ipystata.config import config_stata, config_syntax_higlight
 config_stata('/usr/local/stata15/stata-se')
+
+# syntax highlighting
 # config_syntax_higlight(True)
