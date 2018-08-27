8ea01aa940131d3f784cf70fb2ef738550d7236a
William Hearn <sylus1984@gmail.com>
2018-08-27 08:46:01 -0400

    feat(saslocal): SAS all in one container

---
+++ saslocal/scripts/start-notebook.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -e
+
+/usr/local/SASHome/SASFoundation/9.4/utilities/bin/setuid.sh
+
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+  # launched by JupyterHub, use single-user entrypoint
+  exec /usr/local/bin/start-singleuser.sh $*
+else
+  if [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+    . /usr/local/bin/start.sh jupyter lab $*
+  else
+    . /usr/local/bin/start.sh jupyter notebook $*
+  fi
+fi
